8f5e4395c96fee5bf1c708b116af6ff6a25af235
amit-sgwn <amitsangwan898@gmail.com>
2016-12-29 22:49:30 +0530

    Added article model class and created sqlite database

--- article/models.py
+++ article/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    pub_date = models.DateTimeField('date published')
+    likes = models.IntegerField()
+
+    
